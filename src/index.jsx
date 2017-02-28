abe4fa04023684e548c511f088415349ebd53715
weedma <vydmantas.kolduba@gmail.com>
2017-02-28 20:24:46 +0200

    added redux dev tools

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
-
-let store = createStore(todoApp)
+const enchancers = compose (
+  window.devToolsExtension ? window.devToolsExtension(): f=>f
+);
+let store = createStore(todoApp,enchancers)
 console.log(store.getState());
 render(
   <Provider store={store}>
