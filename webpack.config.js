96b336f1038e0210bcf07a75f34c00d2c667b43e
weedma <vydmantas.kolduba@gmail.com>
2017-03-04 23:22:01 +0200

    added additional styling dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
         loaders: [
             { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
             { test: /\.css$/,  loader: "style-loader!css-loader" },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
     },
     resolve: {
